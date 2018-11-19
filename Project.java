a61b5ff2f0fb460d062722308e56460c889b5778
Nick Paladino <nickp@ncpholdings.org>
2018-11-19 12:36:00 -0600

    Added Helpers and added to Project

--- Project.java
+++ Project.java
@@ -4,15 +4,30 @@ public class Project
 {
 	public static void main(String[] args)
 	{
-		Scanner in = new Scanner(System.in);
+		Scanner stdin = new Scanner(System.in);
+		int selected = -1;
+
 		Inventory inv = new Inventory();
-		Item fruit = new Item("Apple", 2.98);
+		Item apple = new Item("Apple", 2.98);
+		Item alcohol = new Item("Vodka", 10.00);
+		Item pie = new Item("Pie", 13.23);
+		Item bread = new Item("Bread", 5.00);
 
-		int choice = -1;
-
-		inv.put(fruit, 3);
+		inv.put(apple, 10);
+		inv.put(alcohol, 10);
+		inv.put(pie, 10);
+		inv.put(bread, 10);
 		
 		displayMenu();
+		System.out.print("Choice: ");
+
+		do
+		{
+			selected = Helpers.extractUnsignedInt(stdin.nextLine());
+		} while (selected != 1);
+
+		
+
 		System.out.println(inv);
 	}
 
@@ -20,4 +35,6 @@ public class Project
 	{
 		System.out.println("1: Start Checkout");
 	}
+
+	static void 
 }
