eb5a36dff32752de47c080e7a96a0c05148634b6
Gavin <gavin.house@ttu.edu>
2018-11-26 19:23:56 -0600

    added view/update inventory

--- Manager.java
+++ Manager.java
@@ -35,6 +35,54 @@ public class Manager {
         }
     }
 
+    private static void viewOrUpdate(){
 
+        Scanner stdin = new Scanner(System.in);
+        Inventory inv = Inventory.getInstance();
+        int item;
+        char choice;
+
+        while(true){
+            System.out.println(inv);
+            System.out.println("Select item or enter '0' to exit");
+            System.out.println("What item would you like to view/update: ");
+
+            item = stdin.nextInt() - 1;
+            stdin.nextLine();
+            if(item < 0){
+                return;
+            }
+            printItem(inv, item);
+
+            System.out.println("Change: (N)ame/(Q)uantity/(P)rice or (B)ack");
+            choice = Character.toUpperCase(stdin.nextLine().charAt(0));
+
+            switch(choice){
+                case 'N':
+                    System.out.println("New name: ");
+                    inv.names.set(item, stdin.nextLine());
+                    break;
+                case 'Q':
+                    System.out.println("New quantity: ");
+                    inv.amounts.set(item, stdin.nextInt());
+                    stdin.nextLine();
+                    break;
+                case 'P':
+                    System.out.println("New price: ");
+                    inv.prices.set(item, stdin.nextFloat());
+                    stdin.nextLine();
+                    break;
+                case 'B':
+                    break;
+            }
+
+        }
+    }
+
+    private static void printItem(Inventory inv, int itemIndex){
+        System.out.printf("Name: %s\n", inv.names.get(itemIndex));
+        System.out.printf("Quantity: %d\n", inv.amounts.get(itemIndex));
+        System.out.printf("Price: %f\n", inv.prices.get(itemIndex));
+    }
 
 }
