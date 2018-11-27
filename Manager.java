f95b129d0371f9429f3982f649af82e3aa47b4d4
Gavin <gavin.house@ttu.edu>
2018-11-26 18:53:02 -0600

    added menu method

--- Manager.java
+++ Manager.java
@@ -1,4 +1,39 @@
+import java.util.*;
+
 public class Manager {
+
+    static void menu(){
+
+        Scanner stdin = new Scanner(System.in);
+
+        while (true) {
+
+            System.out.println("Welcome!\n");
+            System.out.println("0: Exit");
+            System.out.println("1: View/Update Inventory");
+            System.out.println("2: Print Daily Report");
+            System.out.println("3: Print Inventory Report");
+            System.out.println("Action: ");
+
+            switch (stdin.nextInt()) {
+
+                case 0:
+                    return;
+                case 1:
+                    viewOrUpdate();
+                    break;
+                case 2:
+                    //Print option
+                    break;
+                case 3:
+                    //Print option
+                    break;
+                default:
+                    System.out.println("Input not valid, try again");
+
+            }
+        }
+    }
 
 
 
