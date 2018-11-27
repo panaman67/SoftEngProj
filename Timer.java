444347b395393264c35cd1caae8514974bc42773
Gavin <gavin.house@ttu.edu>
2018-11-26 21:22:55 -0600

    fleshed out printReport

--- Timer.java
+++ Timer.java
@@ -1,4 +1,6 @@
 import java.io.*;
+import java.util.HashMap;
+
 public class Timer {
 
     static void printInventory(){
@@ -22,7 +24,48 @@ public class Timer {
     }
 
     static void printRevenue(){
+        TransactionHistory hist = TransactionHistory.getInstance();
+        try {
+            PrintWriter out = new PrintWriter(new FileWriter("dailyReport.txt"));
+            HashMap<String, Integer> map = prepareItemData();
 
+            for(String s : map.keySet()){
+                out.printf("%s sold: %d", s, map.get(s));
+            }
+
+            out.printf("Total revenue: $%.2f", prepareTotals());
+
+            out.close();
+
+        }
+        catch(IOException e){
+
+        }
+    }
+
+    static HashMap<String, Integer> prepareItemData(){
+        TransactionHistory hist = TransactionHistory.getInstance();
+        HashMap<String, Integer> map = new HashMap<String, Integer>();
+
+        for(String item : hist.items){
+            if(map.containsKey(item)){
+                map.put(item, map.get(item) + 1);
+            }
+            else{
+                map.put(item, 1);
+            }
+        }
+        return map;
+    }
+
+    static float prepareTotals(){
+        TransactionHistory hist = TransactionHistory.getInstance();
+
+        float total = 0;
+        for(Float f : hist.totals){
+            total += f;
+        }
+        return total;
     }
 
 }
