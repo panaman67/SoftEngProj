0483837f63d65e5bfd604a408a9dbfe914ec94d9
Gavin <gavin.house@ttu.edu>
2018-11-26 19:51:03 -0600

    added printInventory method

--- Timer.java
+++ Timer.java
@@ -1,4 +1,24 @@
+import java.io.*;
 public class Timer {
 
+    static void printInventory(){
+        Inventory inv = Inventory.getInstance();
+        try {
+            PrintWriter out = new PrintWriter(new FileWriter("inventoryReport.txt"));
+
+            for (int i = 0; i < inv.names.size(); i++) {
+                out.printf("%s\t%d\t%.2f\n",
+                        inv.names.get(i),
+                        inv.amounts.get(i),
+                        inv.prices.get(i));
+            }
+            out.close();
+        }
+        catch(IOException e){
+
+        }
+
+
+    }
 
 }
