28722e06b08d5b99efd5cfe8bee05b5b678be0f8
Gavin <gavin.house@ttu.edu>
2018-11-26 22:11:59 -0600

    fleshed out pay by credit use case

--- AuthorizationCenter.java
+++ AuthorizationCenter.java
@@ -1,7 +1,22 @@
+import java.util.Random;
+
 public class AuthorizationCenter {
 
-    public boolean authorize(String cardNum){
-        return cardNum.length() == 16;
+    static int authorize(String cardNum, String pin){
+        if(cardNum.length() == 16 && pin.length() == 4){
+            return (new Random()).nextInt(9999);
+        }
+        else{
+            return 0;
+        }
+    }
+    static int authorize(String cardNum){
+        if(cardNum.length() == 16){
+            return (new Random()).nextInt(9999);
+        }
+        else{
+            return 0;
+        }
     }
 
 }
--- Payment.java
+++ Payment.java
@@ -39,8 +39,41 @@ public class Payment
 
 	static boolean payWithCard(float total)
 	{
-		System.out.println("Processing payment....");
-		System.out.println("Payment completed. Have a nice day");
-		return true;
+		Scanner in = new Scanner(System.in);
+		String cardNum, pin;
+
+		System.out.println("Please input your card number, or enter 0 to cancel: ");
+		cardNum = in.nextLine();
+		if(cardNum.equals("0"))
+			return false;
+
+		//check if card is debit
+		if(cardNum.charAt(0) == '1'){
+			System.out.println("Please enter your PIN: ");
+			pin = in.nextLine();
+			int authcode = AuthorizationCenter.authorize(cardNum, pin);
+			if(authcode == 0){
+				System.out.println("Card is not accepted, please enter new payment");
+				return Customer.payBill(total);
+			}
+			else{
+				System.out.println("Payment accepted, printing receipt!");
+				System.out.println(cardNum.substring(11) + Integer.toString(authcode) + "\n\n");
+				return true;
+			}
+		}
+		else{
+			int authcode = AuthorizationCenter.authorize(cardNum);
+			if(authcode == 0){
+				System.out.println("Card declined, ejecting");
+				return false;
+			}
+			else{
+				System.out.println("Payment accepted, printing receipt!");
+				System.out.println(cardNum.substring(11) + Integer.toString(authcode) + "\n\n");
+				return true;
+			}
+
+		}
 	}
 }
